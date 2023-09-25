cdbe6e5ca7c7bbc15aca7627512494248100ef2c
bacalur <128888984+bacalur@users.noreply.github.com>
2023-09-26 00:45:58 +0300

    Update engine.h

--- GameSample/engine.h
+++ GameSample/engine.h
@@ -1,2 +1,15 @@
 #pragma once
 
+#include "board.h"
+#include "player.h"
+
+class engine {
+public:
+    engine(); // Конструктор
+    void run(); // Запуск игры
+
+private:
+    board board_;
+    player xPlayer;
+    player oPlayer;
+};
