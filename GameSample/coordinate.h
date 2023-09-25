9952e7953c4f76c3c6bac14311eb5d25717863ac
bacalur <128888984+bacalur@users.noreply.github.com>
2023-09-26 00:36:16 +0300

    Update coordinate.h

--- GameSample/coordinate.h
+++ GameSample/coordinate.h
@@ -1,2 +1,9 @@
 #pragma once
 
+class Coordinate {
+public:
+    int x; // Координата по горизонтали
+    int y; // Координата по вертикали
+
+    Coordinate(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}
+};
