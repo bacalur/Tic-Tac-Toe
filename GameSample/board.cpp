085e57c0d787a390902d520495e6941196dfc2fa
bacalur <128888984+bacalur@users.noreply.github.com>
2023-09-30 22:53:38 +0300

    Update board.cpp
    Реализация метода

--- GameSample/board.cpp
+++ GameSample/board.cpp
@@ -2,5 +2,9 @@
 #include "coordinate.h"
 
 board::board(){            // конструктор
+  // Инициализация игрового поля пробелами - пустыми клетками
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            gameBoard[i][j] = ' ';
 }
 
