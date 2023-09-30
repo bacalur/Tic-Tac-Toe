2da47d29c9d53c3126b2a56e7551acfa294543cd
bacalur <128888984+bacalur@users.noreply.github.com>
2023-09-30 23:19:48 +0300

    Update player.cpp
    Add realisation of Move function with random choise

--- GameSample/player.cpp
+++ GameSample/player.cpp
@@ -1,5 +1,12 @@
 #include "player.h"
+#include <cstdlib> //    Для функции rand
 
-Coordinate player::Move() {    // Функция Move()
-    // Логика выбора хода для игрока, например, случайный выбор
+coordinate player::Move(const board& gameBoard, char playerSymbol) {    // Функция Move(), "const board& gameBoard" - Ссылка не создает копию объекта, а работает с оригиналом
+    // Логика выбора хода для игрока, случайный выбор
+    int x, y;    // координаты 
+    do {
+        x = std::rand() % 3;
+        y = std::rand() % 3;
+    } while (gameBoard.gameBoard[x][y] != ' ');
+    return coordinate(x, y);
 }
