eaa6c0318cff64e64ce99720f1b8f9dd0214ca5e
bacalur <128888984+bacalur@users.noreply.github.com>
2023-09-30 23:05:35 +0300

    Update engine.cpp
    Add scrin output

--- GameSample/engine.cpp
+++ GameSample/engine.cpp
@@ -8,19 +8,28 @@ void engine::run()          // Функция run() класса "engine"
 {
 	bool xMove = true;        // Ходит ли игрок X или O
 	coordinate move;          // Координата хода
+	char currentPlayerSymbol = 'X';       // Начинает игрок с крестиками
+	
 	while (!gameOver()) {       
 		if (xMove) {      // Ходит ли игрок X
-			move = xPlayer.Move_(board_);
+			std::cout << "Ход игрока X:" << std::endl;
+			move = xPlayer.Move_(board_, currentPlayerSymbol);
 		}
 		else {
-			move = oPlayer.Move_(board_);
+			std::cout << "Ход игрока O:" << std::endl;
+                        move = oPlayer.Move(board_, currentPlayerSymbol);
 		}
 		xMove = !xMove;
-		board.DoMove(move);     // Запись хода игрока(X/O) на доску
+		board.DoMove(move, currentPlayerSymbol);       // Запись хода игрока(X/O) на доску
+                currentPlayerSymbol = (currentPlayerSymbol == 'X') ? 'O' : 'X';    // Смена игрока
 	}
 }
 
 bool engine::gameOver() const       // Функция gameOver() класса "engine"
 {
-	return false;
+    // Проверка на победу	
+
+    // Проверка на ничью
+	
+    return false;   // Игра продолжается
 }
