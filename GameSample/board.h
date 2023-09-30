873fe582bb6111226af45c72fb1dce85c9917cb1
bacalur <128888984+bacalur@users.noreply.github.com>
2023-09-30 22:20:36 +0300

    Update board.h

--- GameSample/board.h
+++ GameSample/board.h
@@ -4,10 +4,10 @@ class board {
 
 public:
         board(); // Конструктор, инициализирующий игровое поле
-	void DoMove(coordinate);
+	void DoMove(coordinate);  // Функция для совершения хода
 
 	friend class engine;
 
 private:
-        char gameBoard[3][3]; // Игровое поле 3x3
+        char gameBoard[3][3]; // Массив - Игровое поле 3x3
 };
