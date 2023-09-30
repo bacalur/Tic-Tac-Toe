bb62f84f18a2c2a48f69de95b884a70b44692de0
bacalur <128888984+bacalur@users.noreply.github.com>
2023-09-30 22:34:55 +0300

    Update engine.cpp

--- GameSample/engine.cpp
+++ GameSample/engine.cpp
@@ -1,26 +1,26 @@
 #include "engine.h"
 
-engine::engine() {
+engine::engine() {          // Конструктор
 
 }
-
-void engine::run()
+ 
+void engine::run()          // Функция run() класса "engine"
 {
-	bool xMove = true;
-	coordinate move;
-	while (!gameOver()) {
-		if (xMove) {
+	bool xMove = true;        // Ходит ли игрок X или O
+	coordinate move;          // Координата хода
+	while (!gameOver()) {       
+		if (xMove) {      // Ходит ли игрок X
 			move = xPlayer.Move_(board_);
 		}
 		else {
 			move = oPlayer.Move_(board_);
 		}
 		xMove = !xMove;
-		board.DoMove(move);
+		board.DoMove(move);     // Запись хода игрока(X/O) на доску
 	}
 }
 
-bool engine::gameOver() const
+bool engine::gameOver() const       // Функция gameOver() класса "engine"
 {
 	return false;
 }
