9127a1a7ea4263660684c97a7361bad5bb30bb2f
bacalur <128888984+bacalur@users.noreply.github.com>
2023-10-03 10:54:47 +0300

    Update player.cpp

--- GameSample/player.cpp
+++ GameSample/player.cpp
@@ -1,12 +1,12 @@
 #include "player.h"
 #include <cstdlib> //    Для функции rand
 
-coordinate player::Move(const board& gameBoard, char playerSymbol) {    // Функция Move(), "const board& gameBoard" - Ссылка не создает копию объекта, а работает с оригиналом
+Coordinate Player::Move(const Board& gameBoard, char playerSymbol) {    // Функция Move(), "const board& gameBoard" - Ссылка не создает копию объекта, а работает с оригиналом
     // Логика выбора хода для игрока, случайный выбор
     int x, y;    // координаты 
     do {
         x = std::rand() % 3;
         y = std::rand() % 3;
-    } while (gameBoard.gameBoard[x][y] != ' ');
-    return coordinate(x, y);
+    } while (gameBoard.GetSymbol(Coordinate(x,y)) != ' ');
+    return Coordinate(x, y);
 }
