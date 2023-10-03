fa50faea1a8781cdf58c14bb8809030342fafc0e
bacalur <128888984+bacalur@users.noreply.github.com>
2023-10-03 10:52:08 +0300

    Update board.h

--- GameSample/board.h
+++ GameSample/board.h
@@ -1,13 +1,16 @@
 #pragma once
+#include "coordinate.h"
 
-class board {
+class Board {
 
 public:
-        board(); // Конструктор, инициализирующий игровое поле
-	void DoMove(coordinate);  // Функция для совершения хода
+	Board(); // Конструктор, инициализирующий игровое поле
+	void DoMove(Coordinate, char);  // Функция для совершения хода
 
 	friend class engine;
 
+	char GetSymbol(Coordinate x) const;
+
 private:
-        char gameBoard[3][3]; // Массив - Игровое поле 3x3
+	char gameBoard[3][3]; // Массив - Игровое поле 3x3
 };
