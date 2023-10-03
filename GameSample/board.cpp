af9601fb6ec2d3874d3b42a361e4c98ad5448394
bacalur <128888984+bacalur@users.noreply.github.com>
2023-10-03 10:51:28 +0300

    Update board.cpp

--- GameSample/board.cpp
+++ GameSample/board.cpp
@@ -1,15 +1,22 @@
 #include "board.h"
 #include "coordinate.h"
 
-board::board(){            // конструктор
-  // Инициализация игрового поля пробелами - пустыми клетками
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            gameBoard[i][j] = ' ';
+Board::Board() {            // конструктор
+	// Инициализация игрового поля пробелами - пустыми клетками
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			gameBoard[i][j] = ' ';
+		}
+	}
 }
 
-void board::DoMove(coordinate coord, char playerSymbol) {
-    // Проверка на корректность координат и свободность клетки
-    if (coord.x >= 0 && coord.x < 3 && coord.y >= 0 && coord.y < 3 && gameBoard[coord.x][coord.y] == ' ') {
-        gameBoard[coord.x][coord.y] = playerSymbol;
-    }
+void Board::DoMove(Coordinate coord, char playerSymbol) {
+	// Проверка на корректность координат и свободность клетки
+	if (coord.x >= 0 && coord.x < 3 && coord.y >= 0 && coord.y < 3 && gameBoard[coord.x][coord.y] == ' ') {
+		gameBoard[coord.x][coord.y] = playerSymbol;
+	}
+}
+
+char Board::GetSymbol(Coordinate x) const {
+	return gameBoard[x.x][x.y];
+}
