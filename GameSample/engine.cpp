1571460958c9779f6a09e2fbea70164b29a15e23
bacalur <128888984+bacalur@users.noreply.github.com>
2023-10-03 10:53:27 +0300

    Update engine.cpp

--- GameSample/engine.cpp
+++ GameSample/engine.cpp
@@ -1,4 +1,5 @@
 #include "engine.h"
+#include <iostream>
 
 engine::engine() {          // Конструктор
  // Инициализация игровой доски в конструкторе
@@ -7,20 +8,20 @@ engine::engine() {          // Конструктор
 void engine::run()          // Функция run() класса "engine"
 {
 	bool xMove = true;        // Ходит ли игрок X или O
-	coordinate move;          // Координата хода
+	Coordinate move;          // Координата хода
 	char currentPlayerSymbol = 'X';       // Начинает игрок с крестиками
 	
 	while (!gameOver()) {       
 		if (xMove) {      // Ходит ли игрок X
 			std::cout << "Ход игрока X:" << std::endl;
-			move = xPlayer.Move_(board_, currentPlayerSymbol);
+			move = xPlayer.Move(board_, currentPlayerSymbol);
 		}
 		else {
 			std::cout << "Ход игрока O:" << std::endl;
                         move = oPlayer.Move(board_, currentPlayerSymbol);
 		}
 		xMove = !xMove;
-		board.DoMove(move, currentPlayerSymbol);       // Запись хода игрока(X/O) на доску
+		board_.DoMove(move, currentPlayerSymbol);       // Запись хода игрока(X/O) на доску
                 currentPlayerSymbol = (currentPlayerSymbol == 'X') ? 'O' : 'X';    // Смена игрока
 	}
 }
