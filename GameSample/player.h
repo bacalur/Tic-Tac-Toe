b81b56b140aa71c586beca7a8e73fe675fe7d497
bacalur <128888984+bacalur@users.noreply.github.com>
2023-10-22 13:49:51 +0300

    Update player.h - Copy Operator

--- GameSample/player.h
+++ GameSample/player.h
@@ -8,5 +8,7 @@ class Player {
 public:
 	Player(char symbol) : symbol(symbol) {} // Где symbol - символ игрока (X или O)	
 
+	Player(const Player& other) : symbol(other.symbol) {}   // Оператор копирования
+
 	Coordinate Move(const Board&, char);    // Функция Move(board)
 };
