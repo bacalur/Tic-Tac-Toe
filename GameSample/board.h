71cd38049bc0c77713720e01d89a37f61e8ba123
bacalur <128888984+bacalur@users.noreply.github.com>
2023-10-22 14:29:16 +0300

    Update board.h

--- GameSample/board.h
+++ GameSample/board.h
@@ -11,6 +11,16 @@ public:
 
 	char GetSymbol(Coordinate x) const;
 
+	Board(const Board& other) {
+        // копия игрового поля (gameBoard) из другого объекта
+        for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            gameBoard[i][j] = other.gameBoard[i][j];
+        }
+        }
+}
+
+
 private:
 	char gameBoard[3][3]; // Массив - Игровое поле 3x3
 };
