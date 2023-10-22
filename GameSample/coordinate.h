d5be1ef49dd2f3ea86e49c8331843f09fd6b0932
bacalur <128888984+bacalur@users.noreply.github.com>
2023-10-22 14:23:53 +0300

    Update coordinate.h - Copy Constructor

--- GameSample/coordinate.h
+++ GameSample/coordinate.h
@@ -6,6 +6,8 @@ public:
     int y; // Координата по вертикали
 
     Coordinate(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}    // Конструктор "Coordinate" со значениями x и y
+
+    Coordinate(const Coordinate& other) : x(other.x), y(other.y) {}    // Конструктор копирования
     
     Coordinate(const Coordinate& other) : x(other.x), y(other.y) {}    // Oператор копирования
 
