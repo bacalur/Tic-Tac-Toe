be024308f6f3350bd4d5019e863edab36be1b9d6
bacalur <128888984+bacalur@users.noreply.github.com>
2023-11-05 14:09:12 +0200

    Update coordinate.h

--- GameSample/coordinate.h
+++ GameSample/coordinate.h
@@ -8,8 +8,6 @@ public:
     Coordinate(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}    // Конструктор "Coordinate" со значениями x и y
 
     Coordinate(const Coordinate& other) : x(other.x), y(other.y) {}    // Конструктор копирования
-    
-    Coordinate(const Coordinate& other) : x(other.x), y(other.y) {}    // Oператор копирования
 
     bool operator==(const Coordinate& other) const {    // Оператор сравненния
     return (x == other.x) && (y == other.y);
