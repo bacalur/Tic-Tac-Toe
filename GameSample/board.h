0d4f4f117122007e1970ae747d1180ab770e0a71
bacalur <128888984+bacalur@users.noreply.github.com>
2023-11-05 15:56:39 +0200

    Update board.h

--- GameSample/board.h
+++ GameSample/board.h
@@ -21,14 +21,6 @@ public:
 	friend class engine;
 
 	char GetSymbol(Coordinate x) const;
-
-	Board(const Board& other) {
-        // копия игрового поля (gameBoard) из другого объекта
-        for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            gameBoard[i][j] = other.gameBoard[i][j];
-        }
-        }
 }
 
 
