5bb272ce419898626a0f7a901c8c0685ad2f4fce
bacalur <128888984+bacalur@users.noreply.github.com>
2023-11-07 19:57:07 +0200

    Update player.h

--- GameSample/player.h
+++ GameSample/player.h
@@ -2,13 +2,16 @@
 
 #include "board.h"
 #include "coordinate.h"
+#include <iostream>
+#include <vector>
 
 class Player {
 
 public:
 	Player(char symbol) : symbol(symbol) {} // Где symbol - символ игрока (X или O)	
-
-	Player(const Player& other) : symbol(other.symbol) {}   // Оператор копирования
+	Player(const Player& other) : symbol(other.symbol) {   // Оператор копирования
+		 return symbol == other.symbol;
+	}
 
 	// Операторы ввода и вывода
 	bool operator==(const Player& other) const {}    // Оператор сравнения
@@ -18,14 +21,13 @@ public:
 	    return os;
 	}
 
-        Player(const Player& other) : symbol(other.symbol) {}
-
         friend std::istream& operator>>(std::istream& is, Player& player) {
         is >> player.symbol;
         return is;
         }
 
-
-
 	Coordinate Move(const Board&, char);    // Функция Move(board)
+
+private:
+    char symbol;
 };
