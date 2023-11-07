a3ac4c4257cd76ba19ecfc0d32cc2d4684ba6854
bacalur <128888984+bacalur@users.noreply.github.com>
2023-11-07 20:03:27 +0200

    Update coordinate.h

--- GameSample/coordinate.h
+++ GameSample/coordinate.h
@@ -1,28 +1,20 @@
 #pragma once
 
+#include <iostream>
+
 class Coordinate {
 public:
     int x; // Координата по горизонтали
     int y; // Координата по вертикали
 
-    Coordinate(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}    // Конструктор "Coordinate" со значениями x и y
+    Coordinate(int xCoord, int yCoord);
+    Coordinate(const Coordinate& other);
 
-    Coordinate(const Coordinate& other) : x(other.x), y(other.y) {}    // Конструктор копирования
-
-    bool operator==(const Coordinate& other) const {    // Оператор сравненния
-    return (x == other.x) && (y == other.y);
-    }
+    bool operator==(const Coordinate& other) const;
 
     // Операторы ввода и вывода
-    friend std::ostream& operator<<(std::ostream& os, const Coordinate& coord) {
-    os << "(" << coord.x << ", " << coord.y << ")";
-    return os;
-    }
-
-    friend std::istream& operator>>(std::istream& is, Coordinate& coord) {
-    is >> coord.x >> coord.y;
-    return is;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const Coordinate& coord);
+    friend std::istream& operator>>(std::istream& is, Coordinate& coord);
 
     Coordinate() = default;
 };
