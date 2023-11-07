a3c80ff5e4d5efee22b0d6cef1c7a0222c22563a
bacalur <128888984+bacalur@users.noreply.github.com>
2023-11-07 20:03:12 +0200

    Update coordinate.cpp

--- GameSample/coordinate.cpp
+++ GameSample/coordinate.cpp
@@ -1,1 +1,19 @@
 #include "coordinate.h"
+
+Coordinate::Coordinate(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}
+
+Coordinate::Coordinate(const Coordinate& other) : x(other.x), y(other.y) {}
+
+bool Coordinate::operator==(const Coordinate& other) const {
+    return (x == other.x) && (y == other.y);
+}
+
+std::ostream& operator<<(std::ostream& os, const Coordinate& coord) {
+    os << "(" << coord.x << ", " << coord.y << ")";
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Coordinate& coord) {
+    is >> coord.x >> coord.y;
+    return is;
+}
