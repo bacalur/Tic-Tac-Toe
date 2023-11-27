39af66f42b5289ef2cd1e05ab9f233505fe6ab62
bacalur <128888984+bacalur@users.noreply.github.com>
2023-11-27 23:13:29 +0200

    Update main.cpp
    Добавила текстовое взаимодействие с игроком + возможность сыграть еще раз

--- GameSample/main.cpp
+++ GameSample/main.cpp
@@ -1,16 +1,31 @@
 #include "board.h"
 #include "engine.h"
 #include "player.h"
+#include <iostream>
 
 int main() {
     // Создаю объекты
     board gameBoard;
     engine gameEngine;
-    player xPlayer;
-    player oPlayer;
+    Player xPlayer('X');
+    Player oPlayer('O');
 
-    // Запуск игры
-    gameEngine.run();
+    char playAgain;
+    do {
+        // Запуск игры
+        gameEngine.run();
+
+        // Вывод текущего состояния доски
+        std::cout << "Игровая доска после завершения игры:\n";
+        std::cout << gameEngine.getBoard();
+
+        // Предложение сыграть еще раз
+        std::cout << "Хочешь сыграть еще раз? (y/n): ";
+        std::cin >> playAgain;
+
+        // Сброс игровой доски для новой игры
+        gameEngine.reset();
+    } while (playAgain == 'y' || playAgain == 'Y');
 
     return 0;
 }
