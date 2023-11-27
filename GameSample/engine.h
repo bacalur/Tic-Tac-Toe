a09ad88a7363ae3d8cc7daa54b09d35cc3cf778a
bacalur <128888984+bacalur@users.noreply.github.com>
2023-11-27 23:15:37 +0200

    Update engine.h
    Добавила методы для получения текущего состояния доски и для сброса игры

--- GameSample/engine.h
+++ GameSample/engine.h
@@ -2,12 +2,15 @@
 
 #include "board.h"
 #include "player.h"
+#include <iostream>
 
 class engine {
 public:
     engine();    // Конструктор
     void run();  // Функция - Запуск игры
     bool gameOver() const;
+    std::string getBoard() const;  // Метод для получения текущего состояния доски
+    void reset();  // Метод для сброса игры
 
 private:
     Board board_;    // Доска
